861b6a9b73d54021559abd974945ea4a0cc22f1a
saltppang <saltppang24@nate.com>
2024-10-06 03:27:16 +0900

    go

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
   ],
-  resolve: {
-    alias: {
-      '@': '/src',
-    }
-  }
+  base: process.env.NODE_ENV === 'production'
+  ? '/port2024-vue-frame/'
+  : '/'
 })
